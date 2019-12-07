943f683f3caa7d53977a5d26cb22edfb31de368a
Berta <2262178k@student.gla.ac.uk>
2019-12-07 19:32:40 +0000

    Initial Hello World
    Commiting the initial code

---
+++ ToyRobotSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToyRobotSimulator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
