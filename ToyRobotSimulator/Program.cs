f91226f534450ded53859f4ea4c4adcadba72484
Berta <2262178k@student.gla.ac.uk>
2019-12-08 23:19:41 +0000

    Code refacturing, documentation and unit testing
    Code refactoring: ExecuteMovement method is split into smaller methods to make it less crowded and easier to understand
    Documentation: added docstrings and comments
    Unit Tests: Added unit tests to test the program

--- ToyRobotSimulator/Program.cs
+++ ToyRobotSimulator/Program.cs
@@ -4,172 +4,277 @@ using System.IO;
 
 namespace ToyRobotSimulator
 {
+    /// <summary>
+    /// A class of the Robot object.
+    /// </summary>
     public class Robot
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public string F { get; set; }
+        public int xCoordinate { get; set; }
+        public int yCoordinate { get; set; }
+        public string facing { get; set; }
+        public string instructions { get; set; }
 
     }
-
-    class MainClass
+    /// <summary>
+    /// Main class containing all code for the simulator.
+    /// </summary>
+    public class MainClass
     {
-        public static Robot ToyRobot = new Robot();
-        public static List<string> commands = new List<string>(); //A list that will store all the commands from a file
-        public static string facing;
-        public static Boolean isInitialPlacement = false;
+        public static Robot ToyRobot = new Robot(); 
+        public static List<string> commands = new List<string>(); //List storing instructions to execute
+        public static Boolean isInitialPlacement=false; //A boolean variable to check whether a Toy Robot has initial placement
 
+        /// <summary>
+        /// Main method containing methods to read an input file and execute movement.
+        /// </summary>
+        /// <param name="args">Arguments.</param>
         public static void Main(string[] args)
         {
+            ToyRobot.instructions = "Input1.txt"; //Setting the name of the instructions input file 
 
-            ReadFromFile();
+            ReadFromFile(ToyRobot.instructions); 
 
-            //Initial placement of toy robot
-            if (isInitialPlacement == true)
-            {
-                var placingCoordinates = commands[0].Split(' ')[1].Split(',');
-                var x = Int32.Parse(placingCoordinates[0]);
-                var y = Int32.Parse(placingCoordinates[1]);
-                facing = placingCoordinates[2];
-
-                if(x>=0 && x<=4 && y >= 0 && y <= 4)
-                {
-                    ToyRobot.X = x;
-                    ToyRobot.Y = y;
-                    ToyRobot.F = facing;
-
-                }
-
-                ExecuteMovement();
-
-            }
-            else
-            {
-                Console.WriteLine("Toy Robot was not placed for movement");
-            }
-
-
+            ExecuteMovement();
 
         }
 
-        public static void ReadFromFile()
+        /// <summary>
+        /// Authenticates a user based on a username and password.
+        /// </summary>
+        /// <param name="filename">Name of the input file.</param>
+        public static void ReadFromFile(string fileName)
         {
-            int lineNo = 0;
-            //Reading the input commands
-            using (var reader = new StreamReader("../../InputFiles/Input4.txt", System.Text.Encoding.UTF8))
+            int lineNumber = 0;
+
+            //Setting up the Stream Reader to read from input file
+            using (var reader = new StreamReader("../../InputFiles/" + fileName, System.Text.Encoding.UTF8))
             {
                 string line;
 
+                //Reading each line of the input file
                 while ((line = reader.ReadLine()) != null)
                 {
+                    //Changing input to be all uppser case to avoid invalid input
+                    line = line.ToUpper();
+
+                    //If statement that determines whether instructions contain an initial "Place" command
+                    //If the first letter of the string is "P" that means a "Place" command exists
                     if (line[0].Equals('P'))
                     {
-                        isInitialPlacement = true;
+                        //If Toy Robot has not been placed yet, set its initial position
+                        if (isInitialPlacement == false)
+                        {
+                            PlacementCommand(line, ToyRobot);
+                        }
+  
                         commands.Add(line);
                     }
-                    else if(isInitialPlacement == true)
+                    //If Toy Robot has been initially placed, other commands can be executed.
+                    //This if statement helps ignore any commands before the first "Place" command in the instructions
+                    else if (isInitialPlacement == true)
                     {
                         commands.Add(line);
                     }
-                    lineNo++;
-                    
+
+                    lineNumber++;
+
                 }
             }
-        }
 
+        }
+        /// <summary>
+        /// Executes the instructions which move the Toy Robot.
+        /// </summary>
         public static void ExecuteMovement()
         {
-
-            for(int i=1; i<commands.Count; i++)
+            //If statement to determine whether Toy Robot was initially placed (whether instructions contained at least one "Place" command)
+            if (isInitialPlacement == true)
             {
-                var command = commands[i];
+                isInitialPlacement = false;
 
-                if (command.Equals("MOVE"))
+                for (int i = 1; i < commands.Count; i++)
                 {
-                    if (ToyRobot.F.Equals("NORTH"))
-                    {
-                        MakeAMove(ToyRobot.X, ToyRobot.Y+1, ToyRobot.F);
-                    }
-                    else if (ToyRobot.F.Equals("EAST"))
-                    {
-                        MakeAMove(ToyRobot.X+1, ToyRobot.Y, ToyRobot.F);
-                    }
-                    else if (ToyRobot.F.Equals("SOUTH"))
-                    {
-                        MakeAMove(ToyRobot.X, ToyRobot.Y-1, ToyRobot.F);
-                    }
-                    else
-                    {
-                        MakeAMove(ToyRobot.X-1, ToyRobot.Y, ToyRobot.F);
-                    }
-                }
-                else if (command.Equals("LEFT"))
-                {
-                    if (ToyRobot.F.Equals("NORTH"))
-                    {
-                        ToyRobot.F="WEST";
-                    }
-                    else if (ToyRobot.F.Equals("EAST"))
-                    {
-                        ToyRobot.F = "NORTH";
-                    }
-                    else if (ToyRobot.F.Equals("SOUTH"))
-                    {
-                        ToyRobot.F = "EAST";
-                    }
-                    else
-                    {
-                        ToyRobot.F = "SOUTH";
-                    }
-                }
-                else if (command.Equals("RIGHT"))
-                {
-                    if (ToyRobot.F.Equals("NORTH"))
-                    {
-                        ToyRobot.F = "EAST";
-                    }
-                    else if (ToyRobot.F.Equals("EAST"))
-                    {
-                        ToyRobot.F = "SOUTH";
-                    }
-                    else if (ToyRobot.F.Equals("SOUTH"))
-                    {
-                        ToyRobot.F = "WEST";
-                    }
-                    else
-                    {
-                        ToyRobot.F = "NORTH";
-                    }
-                }
-                else if (command.Equals("REPORT"))
-                {
-                    Console.WriteLine(ToyRobot.X+","+ToyRobot.Y+","+ToyRobot.F);
-                }
-                else
-                {
-                    var placingCoordinates = commands[0].Split(' ')[1].Split(',');
-                    var x = Int32.Parse(placingCoordinates[0]);
-                    var y = Int32.Parse(placingCoordinates[1]);
-                    facing = placingCoordinates[2];
-                    MakeAMove(x, y, facing);
+                    var command = commands[i];
 
+                    if (command.Equals("MOVE"))
+                    {
+
+                        MoveCommand(ToyRobot.facing, ToyRobot);
+
+                    }
+                    else if (command.Equals("LEFT"))
+                    {
+
+                        LeftCommand(ToyRobot.facing, ToyRobot);
+
+                    }
+                    else if (command.Equals("RIGHT"))
+                    {
+
+                        RightCommand(ToyRobot.facing, ToyRobot);
+
+                    }
+                    else if (command.Equals("REPORT"))
+                    {
+                        //Writes output into console
+                        Console.WriteLine(ReportCommand(ToyRobot));
+                    }
+                    else if (command[0].Equals('P'))
+                    {
+
+                        PlacementCommand(command, ToyRobot);
+
+                    }
                 }
+            }
+            //Throws exception if and only if no "Place" command was found in the input file
+            else
+            {
+                throw new Exception("Toy Robot was not placed for movement");
+
             }
 
         }
 
-        public static void MakeAMove(int x, int y, string f)
+        /// <summary>
+        /// Makes Toy Robot step forward towards where it is facing.
+        /// </summary>
+        /// <param name="x">x coordinate if the step would be taken.</param>
+        /// <param name="y">y coordinate if the step would be taken.</param>
+        /// <param name="f">Current facing of the Toy Robot.</param>
+        /// <param name="robot">The object of the Toy Robot which would be taking the step.</param>
+        public static void MakeAStep(int x, int y, string f, Robot robot)
         {
-
+            //If statement that determines whether the step would make the Toy Robot fall
             if (x >= 0 && x <= 4 && y >= 0 && y <= 4)
             {
-                ToyRobot.X = x;
-                ToyRobot.Y = y;
-                ToyRobot.F = f;
+                robot.xCoordinate = x;
+                robot.yCoordinate = y;
+                robot.facing = f;
 
             }
 
         }
 
+        /// <summary>
+        /// Executes a "Move" command. A "Move" command makes Toy Robot take one step toward the side it is facing.
+        /// </summary>
+        /// <param name="currentFacing">Current facing of the Toy Robot.</param>
+        /// <param name="robot">The object of the Toy Robot which would be taking the step.</param>
+        public static void MoveCommand(string currentFacing, Robot robot)
+        {
+            if (currentFacing.Equals("NORTH"))
+            {
+                MakeAStep(robot.xCoordinate, robot.yCoordinate + 1, robot.facing, robot);
+            }
+            else if (currentFacing.Equals("EAST"))
+            {
+                MakeAStep(robot.xCoordinate + 1, robot.yCoordinate, robot.facing, robot);
+            }
+            else if (currentFacing.Equals("SOUTH"))
+            {
+                MakeAStep(robot.xCoordinate, robot.yCoordinate - 1, robot.facing, robot);
+            }
+            else if (currentFacing.Equals("WEST"))
+            {
+                MakeAStep(robot.xCoordinate - 1, robot.yCoordinate, robot.facing, robot);
+            }
+        }
+
+        /// <summary>
+        /// Executes a "Left" command. A "Left" command changes Toy Robot's facing to the one on the left.
+        /// </summary>
+        /// <param name="currentFacing">Current facing of the Toy Robot.</param>
+        /// <param name="robot">The object of the Toy Robot which would be taking the step.</param>
+        public static void LeftCommand(string currentFacing, Robot robot)
+        {
+            if (currentFacing.Equals("NORTH"))
+            {
+                robot.facing = "WEST";
+            }
+            else if (currentFacing.Equals("EAST"))
+            {
+                robot.facing = "NORTH";
+            }
+            else if (currentFacing.Equals("SOUTH"))
+            {
+                robot.facing = "EAST";
+            }
+            else if (currentFacing.Equals("WEST"))
+            {
+                robot.facing = "SOUTH";
+            }
+        }
+
+        /// <summary>
+        /// Executes a "Right" command. A "Right" command changes Toy Robot's facing to the one on the right.
+        /// </summary>
+        /// <param name="currentFacing">Current facing of the Toy Robot.</param>
+        /// <param name="robot">The object of the Toy Robot which would be taking the step.</param>
+        public static void RightCommand(string currentFacing, Robot robot)
+        {
+            
+            if (currentFacing.Equals("NORTH"))
+            {
+                robot.facing = "EAST";
+            }
+            else if (currentFacing.Equals("EAST"))
+            {
+                robot.facing = "SOUTH";
+            }
+            else if (currentFacing.Equals("SOUTH"))
+            {
+                robot.facing = "WEST";
+            }
+            else if (currentFacing.Equals("WEST"))
+            {
+                robot.facing = "NORTH";
+            }
+
+        }
+        /// <summary>
+        /// Executes a "Report" command. A "Report" command retrieves the output formatted in a desired way.
+        /// </summary>
+        /// <param name="robot">The object of the Toy Robot which would be taking the step.</param>
+        /// <returns>
+        /// String formatted in a desired way.
+        /// </returns>
+        public static string ReportCommand(Robot robot)
+        {
+
+            return robot.xCoordinate + "," + robot.yCoordinate + "," + robot.facing;
+        }
+
+        /// <summary>
+        /// Executes a "Place" command. A "Place" command positions Toy Robot in the supplied coordinates.
+        /// </summary>
+        /// <param name="placeCommand">String containing the name of the command and coordinates with facing.</param>
+        /// <param name="robot">The object of the Toy Robot which would be taking the step.</param>
+        public static void PlacementCommand(string placeCommand, Robot robot)
+        {
+            //String is split on commas
+            var placingCoordinates = placeCommand.Split(' ')[1].Split(',');
+            var coordinateX = Int32.Parse(placingCoordinates[0]);
+            var coordinateY = Int32.Parse(placingCoordinates[1]);
+            var facing = placingCoordinates[2];
+
+            //If statement that checks whether the supplied coordinates are valid
+            if (coordinateX >= 0 && coordinateX <= 4 && coordinateY >= 0 && coordinateY <= 4)
+            {
+                robot.xCoordinate = coordinateX;
+                robot.yCoordinate = coordinateY;
+                robot.facing = facing;
+
+                isInitialPlacement = true;
+                
+                
+
+            }
+            //Throws exception if and only if the coordinates supplied are not valid
+            else
+            {
+                throw new Exception("Coordinates supplied in a 'PLACE' command are invalid");
+            }
+        }
     }
 }
---
+++ ToyRobotSimulatorTests/TestRobotToySimulator.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using System;
+using ToyRobotSimulator;
+
+namespace ToyRobotSimulatorTests
+{
+    [TestFixture()]
+    public class TestRobotToySimulator
+    {
+        [Test()]
+        public void Should_Throw_Exception_When_Robot_Not_Placed()
+        {
+            // Arrange
+            Robot testRobot = new Robot();
+
+            // Act
+            var ex = Assert.Throws<Exception>(() => MainClass.ExecuteMovement());
+
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("Toy Robot was not placed for movement"));
+        }
+        [Test()]
+        public void Should_Throw_Exception_When_Place_Coordinates_Are_Invalid()
+        {
+            // Arrange
+            Robot testRobot = new Robot();
+
+            // Act
+            var ex = Assert.Throws<Exception>(() => MainClass.PlacementCommand("PLACE 6,8,SOUTH", testRobot));
+
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("Coordinates supplied in a 'PLACE' command are invalid"));
+
+        }
+        [Test()]
+        public void Should_Ignore_Move_When_Step_Causes_Fall()
+        {
+            // Arrange
+            Robot testRobot = new Robot();
+            testRobot.xCoordinate = 0;
+            testRobot.yCoordinate = 0;
+            testRobot.facing = "NORTH";
+            var expected = "0,0,WEST";
+
+            // Act
+            MainClass.LeftCommand(testRobot.facing, testRobot);
+            MainClass.MoveCommand(testRobot.facing, testRobot);
+            var actual = MainClass.ReportCommand(testRobot);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [Test()]
+        public void Should_Turn_Left_On_Left_Command()
+        {
+            // Arrange
+            Robot testRobot = new Robot();
+            testRobot.xCoordinate = 0;
+            testRobot.yCoordinate = 0;
+            testRobot.facing = "SOUTH";
+            var expected = "0,0,EAST";
+
+            // Act
+            MainClass.LeftCommand(testRobot.facing, testRobot);
+            var actual = MainClass.ReportCommand(testRobot);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [Test()]
+        public void Should_Turn_Right_On_Right_Command()
+        {
+            // Arrange
+            Robot testRobot = new Robot();
+            testRobot.xCoordinate = 0;
+            testRobot.yCoordinate = 0;
+            testRobot.facing = "SOUTH";
+            var expected = "0,0,WEST";
+
+            // Act
+            MainClass.RightCommand(testRobot.facing, testRobot);
+            var actual = MainClass.ReportCommand(testRobot);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [Test()]
+        public void Should_Step_Forward_On_Move_Command()
+        {
+            // Arrange
+            Robot testRobot = new Robot();
+            testRobot.xCoordinate = 1;
+            testRobot.yCoordinate = 1;
+            testRobot.facing = "NORTH";
+            var expected = "1,2,NORTH";
+
+            // Act
+            MainClass.MoveCommand(testRobot.facing, testRobot);
+            var actual = MainClass.ReportCommand(testRobot);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [Test()]
+        public void Should_Return_Position_On_Report_Command()
+        {
+            // Arrange
+            Robot testRobot = new Robot();
+            testRobot.xCoordinate = 1;
+            testRobot.yCoordinate = 1;
+            testRobot.facing = "WEST";
+            var expected = "1,1,WEST";
+
+            // Act
+            var actual = MainClass.ReportCommand(testRobot);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
